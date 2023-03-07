a83440aebf75ecb1a7fbd423ad24608c00b2176d
BAlimbekov <alimbekovbekmambet@gmail.com>
2023-03-07 17:49:06 -0600

    implemented coinflipper feature

--- src/CoinContainer.jsx
+++ src/CoinContainer.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import { choice } from "./helpers"
+import Coin from "./Coin"
 
 class CoinContainer extends Component {
     static defaultProps = {
         coins: [
             {side: "heads", imgSrc: "https://tinyurl.com/react-coin-heads-jpg"},
-            {side: "tails", imgSrc: "https://tinyurl.com/react-coin-tails-jpg"}
+            {side: "tails", imgSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwMl7xzFXISn5tQkYTjpKcR9LN8IqMYvCW7TZy1EBZpcbknUQqrDMjCNdtYq0hyCDyXco&usqp=CAU"}
         ]
     };
     constructor(props) {
@@ -20,7 +22,14 @@ class CoinContainer extends Component {
 
     flipCoin(){
         const newCoin = choice(this.props.coins);
-        this.setState
+        this.setState(st => {
+            return{
+                currCoin: newCoin,
+                nFlips: st.nFlips + 1,
+                nHeads: st.nHeads + (newCoin.side === "heads" ? 1 : 0),
+                nTails: st.nTails + (newCoin.side === "tails" ? 1 : 0)
+            };
+        })
     }
 
     handleClick(e) {
@@ -30,6 +39,7 @@ class CoinContainer extends Component {
         return (
             <div className="CoinContainer">
                 <h2>Let's Flip A Coin!</h2>
+                {this.state.currCoin && <Coin info= {this.state.currCoin} />}
                 <button onClick= {this.handleClick}>Flip Me!</button>
                 <p>Out of {this.state.nFlips} flips, there have been {this.state.nHeads} heads and {this.state.nTails} tails.</p>
             </div>
